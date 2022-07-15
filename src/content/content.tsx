96c0e96e66f96f7786344d9a889977bdcbf9595b
Ivan Bovin <ivan.a.bovin@gmail.com>
2022-07-15 16:16:08 +0300

    css

--- src/content/content.tsx
+++ src/content/content.tsx
@@ -6,10 +6,6 @@ import { App } from './app';
 let init = false;
 
 function render() {
-  const script = document.createElement('script');
-  script.src = 'https://cdnjs.cloudflare.com/ajax/libs/ace/1.7.1/ace.js';
-  document.body.appendChild(script);
-
   const root = document.createElement('div');
   root.classList.add('jam');
   document.body.append(root);
